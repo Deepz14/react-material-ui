71d3a766eb8c3f974e48e27a4e3ab559d9e81ff0
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-07-08 16:12:51 +0530

    project setup completed

---
+++ src/pages/Create.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import Container from '@mui/material/Container';
+
+export default function Create() {
+  return (
+    <Container>
+      <Typography 
+        variant="h6" 
+        color="textSecondary"
+        component="h2"
+        gutterBottom >
+        Create a New Note
+      </Typography>
+      <Button
+        type="submit"
+        variant="contained" 
+        color="secondary"
+        endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </Container>
+  )
+}
